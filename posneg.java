120c22a9115ff22344c2b69d30c6d53f6cafcc1c
aariyamei <meiyarasurcc@gmail.com>
2017-06-13 14:35:49 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,26 @@
+package program1;
+
+import java.util.Scanner;
+
+public class posneg {
+	public static void main(String args[]){
+		int a;
+		
+		System.out.println("enter the number:");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		if(a>0){
+			System.out.println("the given nym is positive");
+		}
+			 if(a<0){
+				System.out.println(" the given num is negative");
+			 }
+			 if(a==0){
+				 System.out.println("the given num is zero");
+			 }
+				
+			}
+		}
+		
+		
+	
