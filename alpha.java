14144f06196f407bd540af81d6823759f8c584be
aariyamei <meiyarasurcc@gmail.com>
2017-06-14 10:04:08 +0530

    Add files via upload

--- alpha.java
+++ alpha.java
@@ -1,5 +1,18 @@
 package guvi;
 
+import java.util.Scanner;
+
 public class alpha {
+	public static void main(String args[]){
+    	Scanner s=new Scanner(System.in);
+    	char ch ;
+    	System.out.println("enter a character :");
+    	 ch=s.next().charAt(0);
+		if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+    		System.out.println("alphabet");
+    	}
+    	else 
+    		System.out.print("not a alphabet");
+    	}
 
 }
