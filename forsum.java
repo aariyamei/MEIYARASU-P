b0b75cbce489424da822dd5acad9e66fde7c48ba
aariyamei <meiyarasurcc@gmail.com>
2017-06-14 15:17:03 +0530

    Add files via upload

---
+++ forsum.java
@@ -0,0 +1,15 @@
+package guvi;
+
+public class forsum {
+	int sum = 0;
+	int n=10;
+	{
+	for(int i = 1;i <= n;i++)
+	{
+		sum = sum + i;
+	}
+	System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+}
+}
+
+
