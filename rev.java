4b5df52de276ead9fd1e8afe223e60aa0676d05e
aariyamei <meiyarasurcc@gmail.com>
2017-06-26 15:42:42 +0530

    Add files via upload

--- rev.java
+++ rev.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class rev {
 	public static void main(String args[]){
 	String s1 ;
-	String s2;
 	 Scanner in=new Scanner(System.in);
 	 s1=in.next();
 	 String rev=new StringBuffer(s1).reverse().toString();
