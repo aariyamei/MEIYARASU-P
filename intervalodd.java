9a2da53478ea625c198b128a5f4fedbcbf504d3a
aariyamei <meiyarasurcc@gmail.com>
2017-07-03 14:03:06 +0530

    Add files via upload

--- intervalodd.java
+++ intervalodd.java
@@ -7,15 +7,14 @@ public static void main(String args[]){
 	System.out.println("enter the two intervals");
 	a=in.nextInt();
 	b=in.nextInt();
-	for(int i=a;i<b;i++){
-		if(i%2==1){
+	for(int i=a;i<=b;i++){
+		if(i%2!=0){
 			System.out.println(i);
-		}
-	}
-
-	}
-	
-	
-	
 }
+}
+}
+}	
+	
+	
+
 
